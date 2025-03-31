8cd080513e133b4ceb28a736a75a9793e8e28b65
WeiShiuan <zenfonlee@gmail.com>
2025-03-31 21:06:37 +0800

    request not defined fixed

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,5 +1,11 @@
 import 'intersection-observer';
 import { TextEncoder, TextDecoder } from 'util';
+import 'whatwg-fetch';
+
+// 全局註冊
+global.fetch = fetch;
+global.Request = Request;
+global.Response = Response;
 
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
